6794be4ad31a7c35ee38a84282c6a35613d5a755
Josh <joshjbarnes@hotmail.co.uk>
2019-04-03 21:52:13 +0100

    .

--- wk2ex10.php
+++ wk2ex10.php
@@ -1,4 +1,4 @@
-<?
+<?php
   $mymarks["year 1"] = 55;
   $mymarks["year 2"] = 65;
   $mymarks["year 3"] = 75;
@@ -7,5 +7,5 @@
   {
     echo "for  $index  my grade was  $value <br/>";
   }
-  echo "<br/> My best year was Year 3 when I averaged ". $mymarks[“year 3"];
+  echo "<br/> My best year was Year 3 when I averaged ". $mymarks["year 3"];
 ?>
