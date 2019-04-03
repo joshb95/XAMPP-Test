1b226a4abbd1756f1aab5ef7fc05f80da84b8165
Josh <joshjbarnes@hotmail.co.uk>
2019-04-03 20:57:35 +0100

    Inverted Commas

--- wk2ex3.php
+++ wk2ex3.php
@@ -2,7 +2,7 @@
 	define("PASS_GRADE",40);
 	echo "You need " . PASS_GRADE . " % or more to pass. <br/>";
 	$grade = PASS_GRADE  + 15;
-	echo "To achieve a merit you need $grade %  or more <br/>";
+	echo "To achieve a merit you need" $grade "%  or more <br/>";
 	$grade = PASS_GRADE  + 30
-	echo "To achieve a distinction you need $grade %  or more <br/>";
+	echo "To achieve a distinction you need" $grade "%  or more <br/>";
 ?>
