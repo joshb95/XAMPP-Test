6dbd181d6499d4a68e7abc0aaed0bb71fe10267d
Josh <joshjbarnes@hotmail.co.uk>
2019-04-05 21:57:53 +0100

    if else

--- wk3ex2action.php
+++ wk3ex2action.php
@@ -7,7 +7,23 @@
   {
 	echo "You are  21 years old or over<br/>";
   }
-  while ($_POST["radsex"] == "male") {
-  echo "You are a male <br/>";
-  } 
+  if ($_POST["radsex"] == "male")
+  {
+	echo "You are a male <br/>";
+  }
+  else {
+    if ($_POST["radsex"] == "female")
+    {
+    echo "You are a female <br/>";
+    }
+    else {
+      if ($_POST["radsex"] == "other")
+      {
+        echo "You do not identify as male or female <br/>";
+      }
+  else {
+    echo "You have not specified a gender";
+  }
+  }
+
 ?>
