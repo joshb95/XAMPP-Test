bf03ee400b1f1f68482f4432f98338d5e21c2b95
Josh <joshjbarnes@hotmail.co.uk>
2019-04-05 21:50:42 +0100

    gender

--- wk3ex2action.php
+++ wk3ex2action.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
   if ($_POST["txtage"] < 21)
   {
 	echo "You are under 21 years old<br/>";
@@ -7,4 +7,16 @@
   {
 	echo "You are  21 years old or over<br/>";
   }
+  if ($_POST["radsex"] == "male")
+  {
+	echo "You are a male <br/>";
+  }
+  if ($_POST["radsex"] == "female")
+  {
+  echo "You are a female <br/>";
+  if ($_POST["radsex"] == "other")
+  {
+  echo "You do not identify as male or female <br/>";
+  }
+
 ?>
