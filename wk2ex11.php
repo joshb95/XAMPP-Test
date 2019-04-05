9bc36185ba8ebe2c19f3a240f8ba51aa45dd69a1
Josh <joshjbarnes@hotmail.co.uk>
2019-04-05 21:22:27 +0100

    Average

--- wk2ex11.php
+++ wk2ex11.php
@@ -10,6 +10,9 @@
   {
     echo "for $index my grade was $value <br/>";
   }
-
+  $total = 0;
+  $total = $total + $mymarks[$index];
+  $average = $total /  6;
+  echo "My average grade was $average";
 
 ?>
