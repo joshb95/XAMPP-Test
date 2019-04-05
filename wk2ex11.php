0717401dbd3dda69a3fc47d02b94c0a1171a3dfb
Josh <joshjbarnes@hotmail.co.uk>
2019-04-05 21:16:50 +0100

    average

---
+++ wk2ex11.php
@@ -0,0 +1,18 @@
+<?
+  $mymarks["C0450"] = 84;
+  $mymarks["C0451"] = 62;
+  $mymarks["C0452"] = 79;
+  $mymarks["C0453"] = 74;
+  $mymarks["C0454"] = 67;
+  $mymarks["C0455"] = 61;
+
+  while(list($index,$value) = each($mymarks));
+  {
+    echo "for  $index  my grade was  $value <br/>";
+  }
+$total = 0;
+$total = $total + $mymarks[$index];
+$average = $total /  6;
+echo "My average grade was $average";   
+
+?>
