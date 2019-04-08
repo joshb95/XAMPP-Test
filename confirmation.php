5f53497dfbdf8ed5135d34a1add0009752adea19
Josh <joshjbarnes@hotmail.co.uk>
2019-04-08 15:37:48 +0100

    fix

--- confirmation.php
+++ confirmation.php
@@ -1,6 +1,5 @@
 <?php
    echo "<h2> Your order qty is $_POST[selqty] </h2></br>";
-   echo "You have chosen $_POST[selsize] widgets";
    $total =$_POST[selqty] * 10;
    echo "Your total is £$total";
    echo "<h2> and the selected colour is $_POST[selcolour].</h2>"
