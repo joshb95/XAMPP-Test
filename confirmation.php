f0a13add5b64d525faae02f765400faa7348ecea
Josh <joshjbarnes@hotmail.co.uk>
2019-04-08 15:16:45 +0100

    semi colons

--- confirmation.php
+++ confirmation.php
@@ -2,6 +2,6 @@
    echo "<h2> Your order qty is $_POST[selqty] </h2></br>";
    echo "<h2> and the selected colour is $_POST[selcolour].</h2>"
   $price = 10;
-  $total = 10 * $_POST["selqty"]
-  echo "Your total is £.$total"
+  $total = $price * $_POST["selqty"];
+  echo "Your total is £.$total";
 ?>
