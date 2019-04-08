4903788a51fdf8074459ae15af6662a2918d8b47
Josh <joshjbarnes@hotmail.co.uk>
2019-04-08 14:08:33 +0100

    break

--- wk5ex1/index.php
+++ wk5ex1/index.php
@@ -6,6 +6,7 @@
 	$stefan->set_name("Stefan Mischook");
 	$jimmy->set_name("Nick Waddles");
 
-	echo "Stefan's full name: " . $stefan->get_name() <br/>;
-	echo "Nick's full name: " . $jimmy->get_name();
+	echo "Stefan's full name: " . $stefan->get_name();
+  echo "<br/>";
+	echo " Nick's full name: " . $jimmy->get_name();
 ?>
