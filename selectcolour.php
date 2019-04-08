c0af4a9c742afb8f254429c39c5d2ad459d8621d
Josh <joshjbarnes@hotmail.co.uk>
2019-04-08 15:06:54 +0100

    hidden fields

--- selectcolour.php
+++ selectcolour.php
@@ -2,7 +2,7 @@
   <head><title>Select colour page</title></head>
     <body>
       <form action="confirmation.php"  method="post">
-	Select the colour for the <?php echo $_POST[selqty] ?> widgets you are ordering
+	Select the colour for the <?php echo $_POST["selqty"] ?> widgets you are ordering
    	<select name="selcolour">
    	  <option>white</option>
 	  <option>red</option>
@@ -11,6 +11,7 @@
 	  <option>blue</option>
         </select>
         <br/><br/>
+        <input type ="hidden" name="selqty" value="<?php echo $_POST["selqty"]?>"/>
         <input type="submit" value="Buy"/>
       </form>
    </body>
