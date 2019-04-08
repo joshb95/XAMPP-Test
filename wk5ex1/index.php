bb69fecf813e15e92b72a53a6eff265079b8e58b
Josh <joshjbarnes@hotmail.co.uk>
2019-04-08 14:06:22 +0100

    folder

--- wk5ex1/index.php
+++ wk5ex1/index.php
@@ -6,6 +6,6 @@
 	$stefan->set_name("Stefan Mischook");
 	$jimmy->set_name("Nick Waddles");
 
-	echo "Stefan's full name: " . $stefan->get_name();
+	echo "Stefan's full name: " . $stefan->get_name() <br/>;
 	echo "Nick's full name: " . $jimmy->get_name();
 ?>
