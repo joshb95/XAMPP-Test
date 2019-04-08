4c2624dbf1f129d446928e8c79655b4e83e7b876
Josh <joshjbarnes@hotmail.co.uk>
2019-04-08 17:02:41 +0100

    fix

--- wk9ex4.php
+++ wk9ex4.php
@@ -1,7 +1,21 @@
 <?php
      $conn = mysqli_connect("localhost", "root", "", "lotto");
+
+     if ($_POST[selweek]){
+       $sql = "select * from lotto where wk=$_POST[selweek];";
+       $result = mysqli_query($sql);
+       $row = mysqli_fetch_array($result);
+
+       echo "Number 1 is  $row[number1]<br/>";
+       echo "Number 2 is  $row[number2]<br/>";
+       echo "Number 3 is  $row[number3]<br/>";
+       echo "Number 4 is  $row[number4]<br/>";
+       echo "Number 5 is  $row[number5]<br/>";
+       echo "Number 6 is  $row[number6]<br/>";
+     }
+     else {
        $sql = "select * from lotto;";
-       $result = mysqli_query($conn,$sql);
+       $result = mysqli_query($sql);
 
        echo "<form action='$_SESSION[PHP_SELF]' method='post' >";
        echo "<br/>Select the lottery week ";
@@ -12,4 +26,5 @@
        echo "</select><br/>";
        echo "<input type='submit' value='Select' />";
        echo "</form>";
+     }
 ?>
