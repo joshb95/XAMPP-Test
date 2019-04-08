619b0c9938f47b1e7cff66a8889cbd408670debe
Josh <joshjbarnes@hotmail.co.uk>
2019-04-08 15:42:19 +0100

    total

--- confirmation.php
+++ confirmation.php
@@ -1,7 +1,7 @@
 <?php
    echo "<h2> Your order qty is $_POST[selqty] </h2></br>";
-   $price = 10
-   $total =$_POST["selqty"] * $price;
+   $total =$_POST["selqty"];
+   $total = $total * 10
    echo "Your total is £$total";
    echo "<h2> and the selected colour is $_POST[selcolour].</h2>"
 ?>
