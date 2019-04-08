ea1bbb9b612e8238e2cbafcf8d8897715d2e4535
Josh <joshjbarnes@hotmail.co.uk>
2019-04-08 15:23:39 +0100

    remove .

--- confirmation.php
+++ confirmation.php
@@ -1,6 +1,6 @@
 <?php
    echo "<h2> Your order qty is $_POST[selqty] </h2></br>";
    $total =$_POST["selqty"] * 10;
-   echo "Your total is £.$total";
+   echo "Your total is £$total";
    echo "<h2> and the selected colour is $_POST[selcolour].</h2>"
 ?>
