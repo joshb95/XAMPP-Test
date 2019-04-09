49f2bba181be151e716e0c035a654d5f0fe64f2a
Josh <joshjbarnes@hotmail.co.uk>
2019-04-09 12:19:12 +0100

    fix

--- wk2ex10.php
+++ wk2ex10.php
@@ -1,7 +1,7 @@
 <?php
-  $mymarks["year 1"] = 55;
-  $mymarks["year 2"] = 65;
-  $mymarks["year 3"] = 75;
+  $mymarks[1] = 55;
+  $mymarks[2] = 65;
+  $mymarks[3] = 75;
 ?>
 <head>
 <title>Data in table</title>
