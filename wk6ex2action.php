2fc5afd3c8c32ab9b86742ff7165d8590519a635
Josh <joshjbarnes@hotmail.co.uk>
2019-04-09 15:12:19 +0100

    ...

--- wk6ex2action.php
+++ wk6ex2action.php
@@ -12,13 +12,13 @@
 <form action=" " method="post">
 
 	Name :
-	<input type=text name=txtname value=<? echo $row[name] ?> readonly />
+	<input type=text name=txtname value=<? echo "$row[name]" ?> readonly />
 	</br>
 	Phone number :
-	<input type=text name=txttelno value=<? echo $row[phone_number] ?> />
+	<input type=text name=txttelno value=<? echo "$row[phone_number]" ?> />
 	</br>
 	Email :
-	<input type=text name=txtemail value=<? echo $row[email] ?> />
+	<input type=text name=txtemail value=<? echo "$row[email]" ?> />
 	</br>
 	<input type=submit name=btnsubmit value="save"/>
 </form>
