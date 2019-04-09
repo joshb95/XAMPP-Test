6e6d836c77196f62ae4d6013d176cc5b91d4efac
Josh <joshjbarnes@hotmail.co.uk>
2019-04-09 16:57:59 +0100

    ..

--- wk6ex3action.php
+++ wk6ex3action.php
@@ -4,4 +4,8 @@ $sql = "UPDATE test SET name = 'txtname'
 SET email = 'txtemail';
 SET phone_number = 'txttelno'";
 $result = mysqli_query($conn,$sql);
+
+$sql = "DELETE FROM test WHERE
+ID = $Id";
+$result = mysqli_query($conn,$sql);
  ?>
