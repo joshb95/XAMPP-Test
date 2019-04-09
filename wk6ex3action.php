e651b8de821af5e3182accd4f7d539cdd07d13a8
Josh <joshjbarnes@hotmail.co.uk>
2019-04-09 15:55:55 +0100

    ...

--- wk6ex3action.php
+++ wk6ex3action.php
@@ -1,7 +1,7 @@
 <?php
 $conn = mysqli_connect("localhost","root","", "db_21717039");
-$sql = "UPDATE test SET name = 'txtName'
-SET email = 'txtEmail';
-SET phone_number = 'txtPhoneNumber'";
+$sql = "UPDATE test SET name = 'txtname'
+SET email = 'txtemail';
+SET phone_number = 'txttelno'";
 $result = mysqli_query($conn,$sql);
  ?>
