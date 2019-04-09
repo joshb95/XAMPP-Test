f7c531210cafa029ce1638f844e073d4d5e13f7e
Josh <joshjbarnes@hotmail.co.uk>
2019-04-09 12:16:38 +0100

    parts

--- wk2ex10.php
+++ wk2ex10.php
@@ -2,10 +2,19 @@
   $mymarks["year 1"] = 55;
   $mymarks["year 2"] = 65;
   $mymarks["year 3"] = 75;
-
-  while(list($index,$value) = each($mymarks))
-  {
-    echo "for  $index  my grade was  $value <br/>";
-  }
-  echo " My best year was Year 3 when I averaged ". $mymarks["year 3"];
 ?>
+<head>
+<title>Data in table</title>
+</head>
+<body>
+<table border=1 align="center">
+<tr><th>Index</th><th>Subject</th></tr>
+<?php
+  for($count = 0;$count < 3;$count++)
+  {
+    echo "<tr><td>$count</td><td>$topmodules[$count]</td></tr>";
+  }
+?>
+</table>
+</body>
+</html>
